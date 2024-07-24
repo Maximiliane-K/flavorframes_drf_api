fe9b990af66e269ab63de8007e077c7c87e05c9f
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-07-24 20:44:57 +0000

    Add views and serializers for posts model

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    """
+    List and create post when logged in.
+    To associate logged in user with post the perform_create method is used.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve and edit post if you are the owner.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        if self.get_object().owner != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this post")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this post")
+        instance.delete()
