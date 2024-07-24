97625264fb340ff6398dab674b117e27609b6e02
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-07-24 20:59:29 +0000

    Add image validation for size and dimensions

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,3 +8,19 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'owner', 'created_at', 'updated_at', 'content', 'image', 'location_link']
         read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 *2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+        )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+        )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+        )
+        return value
+
