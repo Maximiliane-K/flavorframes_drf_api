bea78e32a3ef07512ca9c449937d3a3f5c1e042a
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-08-15 15:16:57 +0000

    Refactor IsOwnerOrReadOnly permission

--- flavorframes_drf_api/permissions.py
+++ flavorframes_drf_api/permissions.py
@@ -2,10 +2,11 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit or delete it.
+    Custom permission to only allow followers (owners of the follow relationship)
+    to edit or delete it.
     """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.owner == request.user
+        return obj.follower == request.user
