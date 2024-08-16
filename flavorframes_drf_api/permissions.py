e53187ebd0c9d3778c37273d2754bce78ec1cebd
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-08-16 14:29:24 +0000

    Update custom permissions to check ownership

--- flavorframes_drf_api/permissions.py
+++ flavorframes_drf_api/permissions.py
@@ -2,11 +2,16 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow followers (owners of the follow relationship)
-    to edit or delete it.
+    Custom permission to allow only owners
+    to edit or delete the object.
     """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.follower == request.user
+        if hasattr(obj, 'owner'):
+            return obj.owner == request.user
+        elif hasattr(obj, 'follower'):
+            return obj.follower == request.user
+        else:
+            return False  
