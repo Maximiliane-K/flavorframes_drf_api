2aad3fc644463153251134a87d6bfb1f1eb7cbbb
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-12-19 13:55:36 +0000

    Update database configuration to connect to an external database with environment variable

--- flavorframes_drf_api/settings.py
+++ flavorframes_drf_api/settings.py
@@ -159,16 +159,16 @@ WSGI_APPLICATION = 'flavorframes_drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
     
 
