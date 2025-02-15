2e7b32e5f22c20d1352d10c3e31a79ad2b385685
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-15 17:58:47 +0100

    Add profile image and optimize like retrieval in PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,29 +2,45 @@ from rest_framework import serializers
 from .models import Post
 from likes.models import Like
 
+
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     like_id = serializers.SerializerMethodField()
     comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
+    profile_image = serializers.ReadOnlyField(
+        source="owner.profile.profile_picture.url")
 
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'content', 'image', 'location_link', 'like_id', 'comments_count', 'likes_count']
+        fields = [
+            'id',
+            'owner',
+            'profile_image',
+            'created_at',
+            'updated_at',
+            'content',
+            'image',
+            'location_link',
+            'like_id',
+            'comments_count',
+            'likes_count'
+        ]
         read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
 
     def get_like_id(self, obj):
-        request = self.context.get('request', None)
+        request = self.context.get("request", None)
         if request and request.user.is_authenticated:
-            like = Like.objects.filter(user=request.user, post=obj).first()
-            return like.id if like else None
+            like = Like.objects.filter(user=request.user, post=obj)
+            return like.first().id if like.exists() else None
         return None
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:  
+        if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.width > 4096:
             raise serializers.ValidationError('Image width larger than 4096px')
         if value.image.height > 4096:
-            raise serializers.ValidationError('Image height larger than 4096px')
+            raise serializers.ValidationError(
+                'Image height larger than 4096px')
         return value
