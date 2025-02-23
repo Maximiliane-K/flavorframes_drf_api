62161c309cbf65243d0ad6f0c56c4579c14878c6
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-23 23:28:32 +0100

    Refactor events views and serializers

--- events/serializers.py
+++ events/serializers.py
@@ -5,3 +5,7 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+    
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['request'].user  
+        return super().create(validated_data)
--- events/views.py
+++ events/views.py
@@ -1,10 +1,15 @@
 from rest_framework import generics
 from .models import Event
 from .serializers import EventSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class EventListView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
