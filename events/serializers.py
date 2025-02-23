09bfba329b597adcfc9958849e45d71b91837739
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-24 00:20:28 +0100

    Refactor events serializer

--- events/serializers.py
+++ events/serializers.py
@@ -2,10 +2,27 @@ from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.id")
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
     class Meta:
         model = Event
-        fields = '__all__'
-    
-    def create(self, validated_data):
-        validated_data['owner'] = self.context['request'].user  
-        return super().create(validated_data)
+        fields = [
+            "id",
+            "owner",
+            "is_owner",
+            "profile_id",
+            "created_at",
+            "updated_at",
+            "title",
+            "description",
+            "event_date",
+            "time",
+            "location",
+            "image",
+        ]
