cdaf24eac56064769949c03f074e43113d7bc3d1
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-23 23:51:15 +0100

    Refactor owner field definition

--- events/models.py
+++ events/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Event(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=255, blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  
     
