f4fcb458c8b5609a6aa3723438f3095a2e773f22
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-26 22:06:13 +0100

    Add profile_image to EventSerializer

--- events/serializers.py
+++ events/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from .models import Event
 
+
 class EventSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    profile_image = serializers.ReadOnlyField(
+        source="owner.profile.profile_picture.url")
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError("Image size larger than 2MB!")
         if value.image.height > 4096:
-            raise serializers.ValidationError("Image height larger than 4096px!")
+            raise serializers.ValidationError(
+                "Image height larger than 4096px!")
         if value.image.width > 4096:
-            raise serializers.ValidationError("Image width larger than 4096px!")
+            raise serializers.ValidationError(
+                "Image width larger than 4096px!")
         return value
 
     def get_is_owner(self, obj):
@@ -24,6 +29,7 @@ class EventSerializer(serializers.ModelSerializer):
             "id",
             "owner",
             "is_owner",
+            "profile_image",
             "created_at",
             "updated_at",
             "title",
